e1d18e045f74a16a7d193bc1451ccaaa4e1b5356
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-04 11:29:58 -0400

    add missing libs

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,10 +10,13 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/im.dino.Dino.svg
 export DESKTOP=/usr/share/applications/im.dino.Dino.desktop
-export DEPLOY_GLIB_NETWORKING=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/dino
+quick-sharun \
+	/usr/bin/dino         \
+	/usr/lib/dino         \
+	/usr/lib/libqlite.so* \
+	/usr/lib/libxmpp-vala.so*
 
 # Additional changes can be done in between here
 
