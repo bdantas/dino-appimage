aeaf306e9f9bedb711ed82df1e92bc38795c49de
GNUser <goodstuff@dantas.airpost.net>
2026-08-04 10:29:20 -0400

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,13 +6,27 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+#export ADD_HOOKS="self-updater.hook"
+#export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export MAIN_BIN=dino
+export DEPLOY_GLIB_NETWORKING=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/dino
+
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
+
+# Deploy dependencies
+quick-sharun 
 
 # Additional changes can be done in between here
 
