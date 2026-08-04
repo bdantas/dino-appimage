755e7f59f73f0f0650307e77279d1fc47f522d70
GNUser <goodstuff@dantas.airpost.net>
2026-08-04 13:42:01 -0400

    Update make-appimage.sh
    deploying glib_networking is required for client to be able to get user's password and connect to xmpp server.

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/im.dino.Dino.svg
 export DESKTOP=/usr/share/applications/im.dino.Dino.desktop
+export DEPLOY_GLIB_NETWORKING=1
 
 # Deploy dependencies
 quick-sharun \
